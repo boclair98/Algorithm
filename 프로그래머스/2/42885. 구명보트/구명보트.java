ac269d07b75685e6f2894e73c562bb180a13b256
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-04-09 10:22:21 +0900

    [level 2] Title: 구명보트, Time: 10.56 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        // System.out.println(list);
+        int start = 0;
+        int end = people.length-1;
+        while (start<=end){
+            if(people[start] + people[end]<=limit){
+                start++;
+                end--;
+                answer++;
+            }else{
+                answer++;
+                end--;
+            }
+        }
+        return answer;
+    }
+}
