23d77ad0a498bb17a5fc408cef8cd76b73e627a7
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-04-15 15:35:04 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        int count = 0;
+        while (true){
+            if(num == 1){
+                break;
+            }
+            if(num % 2 == 0){
+                num = num / 2;
+                answer++;
+            }else{
+                num = num * 3 + 1;
+                answer++;
+            }
+        }
+        if(answer > 500){
+            return -1;
+        }
+        return answer;
+    }
+}
