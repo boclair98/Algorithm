fe661975b4d3577528a9951e8969bc09b8f16841
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-04-28 12:22:22 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        System.out.println(pow(a,b,c));
+    }
+
+    private static long pow(long a, long b,long c) {
+        if( b == 1){
+            return a % c;
+        }
+        long temp = pow(a,b/2,c);
+
+        if(b % 2 == 1){
+            return (temp*temp % c ) * a % c;
+        }
+        return (temp * temp ) % c;
+
+    }
+
+}
