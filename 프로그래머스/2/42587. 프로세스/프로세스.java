b9e0e9cb2decdcee28deded3d9efbdd9c475f7b7
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-05-11 14:42:10 +0900

    [level 2] Title: 프로세스, Time: 4.58 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,9 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
+        //기존 우선순위
         Deque<int[]> q1 = new ArrayDeque<>();
+        //MAX값 
         Deque<Integer> q2 = new ArrayDeque<>();
+        //방문 여부 판단하기위함 visited
+        //처음 location을 방문한 곳으로 판단.
         boolean[] visited = new boolean[101];
+        
         for(int i = 0; i<priorities.length; i++){
             q1.add(new int[]{priorities[i],i});
             q2.add(priorities[i]);
