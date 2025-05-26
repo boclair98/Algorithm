0186c8f6056d61c43efee735db32456653431fbc
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-05-26 15:46:43 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 128 ms, Memory: 16224 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        int n = word.length();
+
+        //1부터 ~최솟 값 까지 완탐
+        //다 방문 했으면 종료
+
+        int start = 0;
+        int idx = 0;
+        while(start++<=30000){
+            String temp = String.valueOf(start);
+            for(int i = 0; i<temp.length(); i++){
+                if(word.charAt(idx) == temp.charAt(i)){
+                    idx++;
+                }
+                if(idx == n){
+                    System.out.println(start);
+                    return;
+                }
+            }
+
+        }
+    }
+}
