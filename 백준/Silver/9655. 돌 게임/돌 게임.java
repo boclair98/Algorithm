1174f77c7d76a9001eb9671a607d251ba7793209
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-06-09 21:47:09 +0900

    [Silver V] Title: 돌 게임, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+        for(int i = 4; i<=n; i++){
+            dp[i] = Math.min(dp[i-1],dp[i-3])+1;
+        }
+        if(dp[n]%2 == 1){
+            System.out.println("SK");
+        }else{
+            System.out.println("CY");
+        }
+    }
+}
+
