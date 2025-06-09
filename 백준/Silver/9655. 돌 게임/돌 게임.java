ba329366111f45caac4106a43cd263cbb12a170e
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-06-09 21:49:44 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,26 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class Main {
+    static int[] dp ;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[1001];
+        dp = new int[1001];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 1;
         for(int i = 4; i<=n; i++){
-            dp[i] = Math.min(dp[i-1],dp[i-3])+1;
+            dp[i] = Math.min(dp[i-1],dp[i-3]) + 1;
+//            System.out.println(dp[i]);
         }
-        if(dp[n]%2 == 1){
+        if(dp[n] % 2 == 1){
             System.out.println("SK");
         }else{
             System.out.println("CY");
         }
     }
-}
 
+
+}
