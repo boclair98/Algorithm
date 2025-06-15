346fd968df851d572ca512132724bd1ddbc0b83e
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-06-15 13:33:52 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 21.42 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String num = Integer.toString(n,k);
+        String aw = "";
+        
+        for(int i = 0; i<num.length(); i++){
+            if(num.charAt(i)!='0'){
+                aw+=num.charAt(i);
+            }else{
+                if(!aw.equals("")){
+                    long nums = Long.parseLong(aw);
+                    if(CheckSosu(nums)){
+                        answer++;
+                    }
+                    aw ="";
+                }
+            }
+        }
+        
+        //마지막 처리
+        if (!aw.equals("")) {
+            long nums = Long.parseLong(aw);
+            if (CheckSosu(nums)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    //소수 판별 
+    public boolean CheckSosu(long num){
+        if(num<=1){
+            return false;
+        }
+        for(int i = 2; i<=Math.sqrt(num); i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
