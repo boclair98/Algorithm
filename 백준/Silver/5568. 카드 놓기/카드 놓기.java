dce0d2b0a19eefa9454c6c08c2fe5d285d6d0a72
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-04 01:26:13 +0900

    [Silver IV] Title: 카드 놓기, Time: 144 ms, Memory: 16960 KB -BaekjoonHub

---
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static List<Integer> box = new ArrayList<>();
+    static Set<Integer> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        boolean[] visited = new boolean[n];
+
+        DFS(0,n,k,list,visited);
+        System.out.println(set.size());
+    }
+
+    private static void DFS(int idx, int n, int k, List<Integer> list, boolean[] visited) {
+        if(idx == k){
+            String answer ="";
+            for (Integer number : box) {
+                answer+=number+"";
+            }
+            int num = Integer.parseInt(answer);
+            if(!set.contains(num)){
+                set.add(num);
+            }
+            return;
+        }
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                box.add(list.get(i));
+                DFS(idx+1,n,k,list,visited);
+                visited[i] = false;
+                box.remove(box.size()-1);
+            }
+        }
+    }
+}
