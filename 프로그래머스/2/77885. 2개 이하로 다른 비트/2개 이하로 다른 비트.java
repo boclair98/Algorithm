0b861a49b5b4037aeec699f826246da436ee99ac
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-09 09:23:56 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 81.40 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,9 +1,18 @@
 import java.util.*;
 
+//어려움 
+//짝수 일 경우에는 뒷자리를 1로만 변경 가능 -> 10(2) -> 11(3) 
+//홀수 일 경우에는 여러 가지 조건이 발생 
+//우선 홀수 에 0이 있을 경우와 없을 경우가 존재 
+//0이 있으면 1101 이 1110
+//홀수/짝수 -> 000000  10 -> 2 100-> 4 5 6 7
+// 2가지 조건이 1111 1011 -> 1101 011 
+// 1111 10111
+
+
 class Solution {
     public List<Long> solution(long[] numbers) {
         List<Long> answer = new ArrayList<>();
-
         for(int i = 0; i < numbers.length; i++){
             long num = numbers[i];
             String numString = Long.toBinaryString(num);
