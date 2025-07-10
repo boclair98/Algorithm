5c40f1f524c92f373c10ab8cbcf4ac9f65224401
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-10 23:58:08 +0900

    [Bronze V] Title: 사파리월드, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a  = Long.parseLong(st.nextToken());
+        long b =  Long.parseLong(st.nextToken());
+        System.out.println(Math.abs(a-b));
+    }
+}
