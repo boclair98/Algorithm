a3568c0a426d84778b0dd449ec91543174e92c75
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-11 09:18:28 +0900

    [level 2] Title: 호텔 대실, Time: 33.46 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 91.5 MB, 시간: 18.94 ms
+메모리: 80.3 MB, 시간: 33.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 10일 23:47:33
+2025년 07월 11일 09:18:26
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,35 +1,46 @@
 import java.util.*;
+//호텔 끝나는 시간과 들어가는 시간을 비교 후 방을 추가할지 갱신할지 
+
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 0;
+        int room = 0;
         Arrays.sort(book_time,(a,b) -> a[0].compareTo(b[0]));
+        
         //시간 저장
         List<Integer> time = new ArrayList<>();
         
        
+        //우선 기존의 머무는 사람들과 비교한다.
+        //끝나는 시간의 방이 있다면 갱신한다.
+        // 없다면 방을 추가한다.(room)
+        //i가0 즉 처음 인사람은 무조건 입장 , 방 추가
         for(int i = 0; i<book_time.length; i++){
             String[] num = book_time[i];
             int start = time_change(num[0]);
             int finish = time_change(num[1])+10;
             
+            
             boolean check = false;
             for(int j = 0; j<time.size(); j++){
-                if(start >= time.get(j)){
-                    time.set(j,finish);
+                int end = time.get(j);
+                if(start >= end){
                     check = true;
-                    break;
+                    //방 시간 갱신.
+                    time.set(j,finish);  
+                    break; 
                 }
             }
                 
             if(!check){
                 time.add(finish);
+                room++;
+                }
             }
-        }
-        
-        
-        return time.size();
+      
+        return room;
     }
     
+    //끝나는 시간을 int형으로 변환.(비교 용이)
     public static int time_change(String t){
         String[] num = t.split(":");
         return Integer.parseInt(num[0]) * 60 + Integer.parseInt(num[1]);
