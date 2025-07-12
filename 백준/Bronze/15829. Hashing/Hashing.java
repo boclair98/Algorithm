bc39a09762d61d8b41102a296c905651a7b6c5ed
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-12 16:21:13 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14208 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        String input = br.readLine();
+        Map<String,Integer> map = new HashMap<>();
+        for(int i = 0; i<alpha.length(); i++){
+            String w = alpha.charAt(i)+"";
+            map.put(w,i+1);
+        }
+//        System.out.println(map);
+        long total = 0;
+        for(int i = 0; i<n; i++){
+            total+=map.get(input.charAt(i)+"")*((long) Math.pow(31,i));
+//            System.out.println(map.get(alpha.charAt(i)+""));
+        }
+        System.out.println(total);
+    }
+}
