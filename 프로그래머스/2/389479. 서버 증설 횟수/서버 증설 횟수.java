f5277ec51317bd34c5f4d03d9529c76c7495b330
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-13 15:13:12 +0900

    [level 2] Title: 서버 증설 횟수, Time: 23.41 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,33 +1,32 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
+        int n = players.length;
         Queue<Integer> time = new ArrayDeque<>();
-
-        for (int i = 0; i < players.length; i++) {
-            // 1. 만료된 서버 제거
-            while (!time.isEmpty() && time.peek() <= i) {
-                time.poll();
+        
+        
+        for(int i = 0; i<n; i++){
+            Iterator<Integer> iter = time.iterator();
+            while (iter.hasNext()) {
+                int val = iter.next();
+                if (val <= i) {
+                    iter.remove(); 
+                }
             }
-
-            // 2. 필요한 서버 수 계산 (올림 처리)
-            int need = (players[i] ) / m;
-
-            // 3. 현재 가동 서버 수
-            int current = time.size();
-
-            // 4. 부족한 서버 개수만큼 추가
-            int toAdd = need - current;
-            for (int j = 0; j < toAdd; j++) {
-                time.offer(i + k);
+            int number = players[i] / m ;
+            int serverSize = time.size();
+            int needSize = number - serverSize;
+            for(int j = 0; j<needSize; j++){
+                time.offer(i+k);
                 answer++;
             }
-
-            // 디버그 출력 (필요 시)
-            System.out.println("time: " + time + ", answer: " + answer);
+            System.out.println(time+" "+answer);
+            
         }
-
+        
         return answer;
     }
+    
+    
 }
