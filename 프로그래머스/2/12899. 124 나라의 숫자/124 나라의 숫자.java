bd1ee02d7ccbb1965ca159c23be6f96dc0ffbe22
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-14 20:15:11 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.05 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,10 +1,11 @@
 import java.util.*;
 class Solution {
-    static String[] nums;
     static StringBuilder sb = new StringBuilder(); ;
     public String solution(int n) {
         String answer = "";
-        nums = new String[]{"4","1","2"};
+        //412로 구분 
+        //나머지가 0으로 끝나면 4로 하기 위해서
+        String[] nums = new String[]{"4","1","2"};
         List<String> word = new ArrayList<>();
         if(n == 1){
             return "1";
@@ -13,9 +14,15 @@ class Solution {
         }else if(n == 3){
             return "4";
         }
+        
         while(n>0){
+            //우선 기존 숫자를 3으로 나눈다.
             int div = n % 3; 
+            
+            //나머지를 추가해준다.
             sb.append(nums[div]);
+            //나머지가 0이라면 n / 3 -1 을 해줘야 겹치지 않는다.
+            //0이 아니라면 계속 3으로 나눠주고 while 문 실행.
             if(div == 0){
                 n = n / 3 -1;
             }else{
@@ -24,7 +31,10 @@ class Solution {
         
         }
         
-        
+        //처음에 나머지지를 기준으로 넘었으니 반환할때는 반대로 
+        // 7 -> 몫2 나머지 1이니-> 1추가
+        // 2 -> 몫 0 나머지 2  -> 2 추가
+        // 반환할떄는 21
         return sb.reverse().toString();
     }
 }
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 0.05 ms
+메모리: 53.7 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 14일 13:44:22
+2025년 07월 14일 20:15:07
 
 ### 문제 설명
 
