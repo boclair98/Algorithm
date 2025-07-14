712790031c702b2d9efd44749482f49bb457db14
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-14 09:03:44 +0900

    [level 2] Title: 서버 증설 횟수, Time: 25.56 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 23.41 ms
+메모리: 84.2 MB, 시간: 25.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 13일 15:13:10
+2025년 07월 14일 09:03:42
 
 ### 문제 설명
 
--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,24 +1,44 @@
 import java.util.*;
+//m명 늘어날 때 마다 서버 1대 추가
+//m명 미만 서버 x
+//n*m명 이상 (n+1)*m 미만이라면 n대의 서버 운영
+//k시간 동안 운영
+//만료된 서버는 제거해주고, 몇대를 서버 증설해야할지 구현
 class Solution {
     public int solution(int[] players, int m, int k) {
+        
         int answer = 0;
-        int n = players.length;
+        int n = 24;
+        //시간을 집어 넣을 큐 
         Queue<Integer> time = new ArrayDeque<>();
         
-        
         for(int i = 0; i<n; i++){
+            // 서버 시간이 만료되면 제거해주기 위한 iterator
             Iterator<Integer> iter = time.iterator();
             while (iter.hasNext()) {
                 int val = iter.next();
-                if (val <= i) {
+                if (val == i) {
                     iter.remove(); 
                 }
             }
-            int number = players[i] / m ;
+            
+            // 몇대의 서버가 필요한지 인원.
+            int server = players[i] / m ;
+            
+            
+            //현재 서버 갯수
             int serverSize = time.size();
-            int needSize = number - serverSize;
+            
+            
+            //실질적으로 추가해야 할 서버 사이즈
+            //음수면 없다고 가정, 양수면 있음.
+            int needSize = server - serverSize;
+            
+            //서버 추가.
             for(int j = 0; j<needSize; j++){
+                //끝나는 시간
                 time.offer(i+k);
+                //서버 한대 추가
                 answer++;
             }
             System.out.println(time+" "+answer);
