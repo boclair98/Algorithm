a1943be3b8a9c388193a756c8c901c508b00f24b
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-14 15:15:09 +0900

    [Gold IV] Title: 음식 평론가, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println(m - gcd(n,m));
+    }
+    private static int gcd(int a, int b){
+        while(b != 0){
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+
+
+}
