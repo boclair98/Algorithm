123b65b0738a5902a647cf6fd59748b848702769
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-16 01:42:58 +0900

    [level 2] Title: 무인도 여행, Time: 24.20 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -0,0 +1,74 @@
+import java.util.*;
+class Solution {
+    static boolean[][] visited;
+    static String[][] map;
+    static int n,m;
+    static int total;
+    static int[] dx ={1,0,-1,0};
+    static int[] dy ={0,1,0,-1};
+    public List<Integer> solution(String[] maps) {
+        List<Integer> answer = new ArrayList<>();
+        n = maps.length;
+        m = maps[0].length();
+        
+        map = new String[n][m];
+        visited = new boolean[n][m];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                map[i][j] = maps[i].charAt(j)+"";
+            }
+        }
+        
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(isNumeric(map[i][j]) && !visited[i][j]){
+                    total = 0;
+                    BFS(i,j);
+                    answer.add(total);
+                }
+            }
+        }
+        if(answer.size() == 0){
+            answer.add(-1);
+        }else{
+            Collections.sort(answer);
+        }
+        
+        return answer;
+    }
+    
+    private static boolean isNumeric(String num){
+
+        for(Character c : num.toCharArray()){
+            if(!Character.isDigit(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private static void BFS(int s,int d){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{s,d});
+        visited[s][d] = true;
+        total+=Integer.parseInt(map[s][d]);
+        while (!q.isEmpty()){
+            int[] now = q.poll();
+            int x = now[0];
+            int y = now[1];
+            for(int i = 0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx<0 || ny < 0 || nx>=n || ny >=m) continue;
+                if(!visited[nx][ny] && !map[nx][ny].equals("X")){
+                    visited[nx][ny] = true;
+                    total+=Integer.parseInt(map[nx][ny]);
+                    q.offer(new int[]{nx,ny});
+                }
+            }
+            
+        }
+        
+    }
+}
