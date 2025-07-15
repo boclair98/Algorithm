cd4a9c296939c0117f56c684ed28cb6e549fa5f4
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-16 01:46:33 +0900

    [level 2] Title: 무인도 여행, Time: 17.62 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,6 +1,8 @@
 import java.util.*;
 class Solution {
+    //방문처리 
     static boolean[][] visited;
+    //2차원 변환
     static String[][] map;
     static int n,m;
     static int total;
@@ -10,9 +12,9 @@ class Solution {
         List<Integer> answer = new ArrayList<>();
         n = maps.length;
         m = maps[0].length();
-        
         map = new String[n][m];
         visited = new boolean[n][m];
+        
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 map[i][j] = maps[i].charAt(j)+"";
@@ -20,11 +22,14 @@ class Solution {
         }
         
         
+        //우선 문자열이 숫자이고 방문하지 않으면 BFS 탐색.
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                if(isNumeric(map[i][j]) && !visited[i][j]){
+                if(!map[i][j].equals("X") && !visited[i][j]){
                     total = 0;
                     BFS(i,j);
+                    // 탐색 후 total에 저장.
+                    // answer에 저장.
                     answer.add(total);
                 }
             }
@@ -38,16 +43,17 @@ class Solution {
         return answer;
     }
     
-    private static boolean isNumeric(String num){
-
-        for(Character c : num.toCharArray()){
-            if(!Character.isDigit(c)){
-                return false;
-            }
-        }
-        return true;
-    }
+    //숫자 함수
+    // private static boolean isNumeric(String num){
+    //     for(Character c : num.toCharArray()){
+    //         if(!Character.isDigit(c)){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     
+    //BFS탐색.
     private static void BFS(int s,int d){
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{s,d});
@@ -61,6 +67,7 @@ class Solution {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 if(nx<0 || ny < 0 || nx>=n || ny >=m) continue;
+                //방문 안했거나 X가 아니면 BFS 탐색.
                 if(!visited[nx][ny] && !map[nx][ny].equals("X")){
                     visited[nx][ny] = true;
                     total+=Integer.parseInt(map[nx][ny]);
