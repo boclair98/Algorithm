d0b05b887e3ed63e071aa073ded59eac2cbab5dd
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-21 00:49:57 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 13.38 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,57 +1,57 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int rows, int columns, int[][] queries) {
-        int[][] matrix = new int[rows][columns];
-        int cnt = 1;
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < columns; j++)
-                matrix[i][j] = cnt++;
+    public List<Integer> solution(int rows, int columns, int[][] queries) {
+        List<Integer> answer = new ArrayList<>();
+        int[][] map = new int[rows][columns];
+        int count = 1;
 
-        int[] answer = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            answer[i] = rotate(matrix, queries[i]);
+        // 1부터 차례대로 초기화
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                map[i][j] = count++;
+            }
         }
+
+        for (int[] query : queries) {
+            int x1 = query[0] - 1;
+            int y1 = query[1] - 1;
+            int x2 = query[2] - 1;
+            int y2 = query[3] - 1;
+
+            int prev = map[x1][y1]; // 이전값 
+            int min = Integer.MAX_VALUE;
+            for(int j = y1+1; j<=y2; j++){
+                int temp = map[x1][j];
+                map[x1][j] = prev;
+                prev = temp;
+                min = Math.min(min,prev);
+            }
+            
+            for(int j = x1+1; j<=x2; j++){
+                int temp = map[j][y2];
+                map[j][y2] = prev;
+                prev = temp;
+                min = Math.min(min,prev);
+            }
+            
+            for(int j = y2-1; j>=y1; j--){
+                int temp = map[x2][j];
+                map[x2][j] = prev;
+                prev = temp;
+                min = Math.min(min,prev);
+            }
+            
+            for(int j = x2-1; j>=x1; j--){
+                int temp = map[j][y1];
+                map[j][y1] = prev;
+                prev = temp;
+                min = Math.min(min,prev);
+            }
+            answer.add(min);
+            
+        }
+
         return answer;
-    }
-
-    // 시계 방향으로 1칸 회전시키고, 이동된 숫자 중 최솟값을 반환
-    private int rotate(int[][] m, int[] q) {
-        int x1 = q[0] - 1, y1 = q[1] - 1;
-        int x2 = q[2] - 1, y2 = q[3] - 1;
-
-        int prev = m[x1][y1];
-        int min = prev;
-
-        // → 방향 (윗변)
-        for (int j = y1 + 1; j <= y2; j++) {
-            int tmp = m[x1][j];
-            m[x1][j] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
-        }
-        // ↓ 방향 (오른쪽변)
-        for (int i = x1 + 1; i <= x2; i++) {
-            int tmp = m[i][y2];
-            m[i][y2] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
-        }
-        // ← 방향 (아랫변)
-        for (int j = y2 - 1; j >= y1; j--) {
-            int tmp = m[x2][j];
-            m[x2][j] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
-        }
-        // ↑ 방향 (왼쪽변)
-        for (int i = x2 - 1; i >= x1; i--) {
-            int tmp = m[i][y1];
-            m[i][y1] = prev;
-            prev = tmp;
-            min = Math.min(min, prev);
-        }
-
-        return min;
     }
 }
