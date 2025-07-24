489268cf00ed92ac266528cdc0edb2821bd200c7
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-24 09:19:16 +0900

    [level 2] Title: 광물 캐기, Time: 7.42 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -3,10 +3,13 @@ class Solution {
     static int min = Integer.MAX_VALUE;
     public int solution(int[] picks, String[] minerals) {
         int answer = 0; 
+        //횟수 
         int count = picks[0] + picks[1] + picks[2];
+        //백트래킹 적용.
         dfs(0,picks,minerals,0,count);
         return min;
     }
+    // 더이상 캘 미네랄이 없거나 곡괭이가 없으면 return;
     static void dfs(int idx, int[] picks, String[] minerals, int total,int count){
         if(idx >= minerals.length || count == 0 ){
             min = Math.min(min,total);
@@ -15,16 +18,20 @@ class Solution {
         for(int i = 0; i<3; i++){
             if(picks[i] > 0 ){
                 int piro = 0;
+                //idx부터 5번 미네랄 다 캘수 있을 때 까지.
                 for(int j = idx; j < idx + 5 && j < minerals.length; j++){
                     String mineral = minerals[j];
+                    //i = 0 다이아몬드
                     if(i == 0){
                         piro+=1;
+                        //i = 1 철
                     }else if(i == 1){
                         if(mineral.equals("diamond")){
                             piro+=5;
                         }else{
                             piro+=1;
                         }
+                        // 돌.
                     }else{
                         if(mineral.equals("diamond")){
                             piro+=25;
