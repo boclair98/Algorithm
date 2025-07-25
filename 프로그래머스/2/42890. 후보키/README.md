a35674a6684478649640e859519928f564385875
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-25 09:12:03 +0900

    [level 2] Title: 후보키, Time: 14.06 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/README.md
+++ 프로그래머스/2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level 2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890) 
 
 ### 성능 요약
 
-메모리: 93 MB, 시간: 16.97 ms
+메모리: 96.8 MB, 시간: 14.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 00:41:48
+2025년 07월 25일 09:12:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -2,6 +2,8 @@ import java.util.*;
 //유일성(uniqueness) : 릴레이션에 있는 모든 튜플에 대해 유일하게 식별되어야 한다.
 //최소성(minimality) : 유일성을 가진 키를 구성하는 속성(Attribute) 중 하나라도 제외하는 경우 유일성이 깨지는 것을 의미한다.
 //즉, 릴레이션의 모든 튜플을 유일하게 식별하는 데 꼭 필요한 속성들로만 구성되어야 한다.
+//여러 조합으로 후보키 만든다.
+
 class Solution {
     static Set<String> set = new HashSet<>();
     static List<Set<Integer>>key = new ArrayList<>();
@@ -19,27 +21,41 @@ class Solution {
 
     public static void dfs(int depth,int start, int end, String[][] relation){
         if(depth == end){
+            
+            //리스트안에 어떤 조합으로 진행할지 선정.
             List<Integer> list = new ArrayList<>();
             for(int i = 0; i<relation[0].length; i++){
-                if(visited[i]) list.add(i);
+                if(visited[i]) list.add(i); // 0
             }
+            
+            
+            //유일성 검사. [0,1,2] 이런식으로 있으면 학번 이름 전공을 문자열로 만들어줌.
+            // 문자열로 만든 후 set에 그 문자열이 있으면 유일성 위반
+            // 없으면 유일성 성립.
             
             Set<String> set = new HashSet<>();
             for(int i = 0; i<relation.length; i++){
                 StringBuilder sb = new StringBuilder();
+                //01 02
                 for(int col : list){
-                    sb.append(relation[i][col]).append("_");
+                    sb.append(relation[i][col]).append("_"); 
                 }
                 if(set.contains(sb.toString())) return;
-                set.add(sb.toString());
+                    set.add(sb.toString());
             }
+            
             System.out.println(set);
+            //최소성 검사. // 질문하기 이용 
+            // 키안에 [1]이 있을 경우 [1,2] 혹은 [1,3] , [1,2,3] 이런게 성립 x
+            
             for(Set<Integer> keys : key){
-                if(list.containsAll(keys)) return;
+                if(list.containsAll(keys)) return; 
             }
-            if(set.size() == relation.length){
-                key.add(new HashSet<>(list));
-            }
+            // System.out.println(key);
+            
+            //유일성과 최소성이 다 성립이 되면 추가
+            key.add(new HashSet<>(list));
+           
             return;
         
         }
