3f6c8d000b4e02a581aee708766f20d2b3d2eb9c
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-29 17:13:13 +0900

    [Gold III] Title: 내리막 길, Time: 348 ms, Memory: 37304 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,24 +1,25 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static int n,m,answer;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    static int n, m ;
+    static int[][] dp;
     static int[][] map;
     static boolean[][] visited;
-    static int[] dx ={1,0,-1,0};
-    static int[] dy ={0,1,0,-1};
-    static int[][]dp ;
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        map = new int[n][m];
         dp = new int[n][m];
+        map = new int[n][m];
         visited = new boolean[n][m];
-
         for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j<m; j++){
@@ -27,36 +28,25 @@ public class Main {
             }
         }
         visited[0][0] = true;
-        System.out.println(Backtracking(0,0));
-
+        System.out.println(dfs(0,0));
     }
 
-    private static int Backtracking(int x, int y) {
-
-        if(x == n-1 && y == m-1){
-            return 1;
-        }
-
-        if(dp[x][y] !=-1){
-            return dp[x][y];
-        }
+    static int dfs(int x, int y){
+        if(x == n-1 && y == m-1) return 1;
+        if(dp[x][y]!=-1) return dp[x][y];
         dp[x][y] = 0;
-
         for(int i = 0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(isRange(nx,ny)){
-                if(!visited[nx][ny] && map[nx][ny] < map[x][y]){
-                    visited[nx][ny] = true;
-                    dp[x][y]+=Backtracking(nx,ny);
-                    visited[nx][ny] = false;
-                }
+            if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
+            if(!visited[nx][ny] && map[nx][ny] < map[x][y]){
+                visited[nx][ny] = true;
+                dp[x][y]+=dfs(nx, ny);
+                visited[nx][ny] = false;
             }
         }
         return dp[x][y];
     }
-    private static boolean isRange(int nx, int ny) {
 
-        return 0<=nx && nx<n && 0<=ny && ny<m;
-    }
 }
+
