d27f6662e2ae800eb4a606926a1d33821e01d175
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-07-30 09:17:38 +0900

    [level 3] Title: 등굣길, Time: 3.32 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -14,15 +14,21 @@ class Solution {
         if(x > n || y > m){
             return 0;
         }
+        
         if(x == n && y == m) {
             return 1;
         }
+        
+        //기존에 들렸던 기억이 있다면 재활용.
         if(dp[x][y] != -1){
             return dp[x][y] ;
         }
+        
+        //물에 잠기면 0 반환
         for(int i = 0; i<puddles.length; i++){
             if(x == puddles[i][1] && puddles[i][0] == y) return 0;
         }
+        
         return dp[x][y] = (dfs(x+1,y,n,m,puddles,dp) + dfs(x,y+1,n,m,puddles,dp))  % 1000000007 ;
     }
 }
