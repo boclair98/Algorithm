1c3ed93674bdf3ad1ea8353875eee9b5fa96e52d
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-08-03 20:52:21 +0900

    [level 3] Title: 숫자 게임, Time: 65.60 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int left = 0;
+        int right = 0;
+        while (left < A.length && right < B.length) {
+            if(A[left] < B[right]){
+                answer++;
+                right++;
+                left++;
+            }else{
+                right++;
+            }
+        }
+        return answer;
+    }
+}
