2a59a59ba4d594947872b64d78aaa7d0f28a1b93
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-08-05 09:05:15 +0900

    [level 3] Title: 여행경로, Time: 74.48 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -4,13 +4,14 @@ class Solution {
     static List<String> airport = new ArrayList<>();
     static int n;
     public List<String> solution(String[][] tickets) {
+        
         List<String> answer = new ArrayList<>();
         n = tickets.length;
         visited = new boolean[n];
-        
         DFS(0,"ICN","ICN",tickets);
         Collections.sort(airport);
         // System.out.println(airport);
+        
         String answers = airport.get(0);
         String[] word = answers.split(" ");
         for(int i = 0; i<word.length; i++){
@@ -19,15 +20,17 @@ class Solution {
         
         return answer;
     }
+    
+    
     public static void DFS(int idx, String start,String route, String[][] tickets){
         if(idx == n){
             airport.add(route);
             return;
         }
+        
         for(int i = 0; i<n; i++){
             if(tickets[i][0].equals(start) && !visited[i]){
                 visited[i] = true;
-                
                 DFS(idx+1,tickets[i][1],route+" "+tickets[i][1], tickets);
                 visited[i] = false;
             }
