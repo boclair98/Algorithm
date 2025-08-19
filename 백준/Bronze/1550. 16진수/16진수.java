875f92613e3b298947cfe4a9571091a11a5437f4
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-08-19 23:39:29 +0900

    [Bronze II] Title: 16진수, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        System.out.println(Integer.parseInt(num,16));
+
+    }
+
+}
+
