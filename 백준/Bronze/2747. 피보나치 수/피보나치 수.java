75f61c0803227e0c627a452690eda5af0c0937f8
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-10-14 12:24:08 +0900

    [Bronze II] Title: 피보나치 수, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,22 +1,28 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n ;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        int[] arr = new int[100001];
-        arr[0] = 0;
-        arr[1] = 1;
-        arr[2] = 1;
-        for(int i = 3; i<=n; i++){
-            arr[i] = arr[i-1] + arr[i-2];
-        }
-        System.out.println(arr[n]);
-
-
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(memoization(n));
     }
 
+    private static int memoization(int n) {
+        if(n<=2){
+            return 1;
+        }
+
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = memoization(n-1) + memoization(n-2);
+
+    }
 
 }
