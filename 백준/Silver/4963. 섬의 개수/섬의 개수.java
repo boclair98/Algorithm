ecc7306da60ee07b4030f424b8ab635fd2bf51f3
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-10-18 23:58:54 +0900

    [Silver II] Title: 섬의 개수, Time: 144 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -3,64 +3,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-
-class Main {
-    static int n,m ;
-    static int[] dx ={1,0,-1,0,-1,1,-1,1};
-    static int[] dy ={0,1,0,-1,-1,-1,1,1};
-    static int[][] maps;
-    static boolean[][] visited;
+public class Main {
     static StringTokenizer st;
+    static int w,h;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1,0,-1,0,-1,-1,1,1};
+    static int[] dy = {0,1,0,-1,-1,1,-1,1};
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       while(true){
-           st = new StringTokenizer(br.readLine());
-           m = Integer.parseInt(st.nextToken());
-           n = Integer.parseInt(st.nextToken());
-           if(n == 0 && m == 0) break;
-           maps = new int[n][m];
-           visited = new boolean[n][m];
-           int count = 0;
-           for(int i = 0 ; i<n; i++){
-               st = new StringTokenizer(br.readLine());
-               for(int j = 0; j<m; j++){
-                   maps[i][j] = Integer.parseInt(st.nextToken());
-               }
-           }
-           for(int i = 0; i < n; i++){
-               for(int j = 0; j<m; j++){
-                   if(maps[i][j] == 1 && !visited[i][j]){
-                       count++;
-                       bfs(i,j);
-                   }
-               }
-           }
-           System.out.println(count);
-
-       }
-
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        st = new StringTokenizer(br.readLine());
+        while(true){
+            int total = 0;
+            st = new StringTokenizer(br.readLine());
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            if(w == 0 && h == 0) break;
+            map = new int[w][h];
+            visited = new boolean[w][h];
+            for (int i = 0; i < w; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < h; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            for(int i = 0 ; i<w; i++){
+                for(int j = 0; j<h; j++){
+                    if(map[i][j] == 1 && !visited[i][j]){
+                        visited[i][j] = true;
+                        bfs(i,j);
+                        total++;
+                    }
+                }
+            }
+            System.out.println(total);
+        }
     }
 
-    private static void bfs(int x1, int y1) {
+    private static void bfs(int sx, int sy) {
         Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{x1,y1});
-        visited[x1][y1] = true;
+        q.add(new int[]{sx,sy});
+//        visited[sx][sy] = true;
         while(!q.isEmpty()){
             int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
+            int x = cur[0],y = cur[1];
             for(int i = 0; i<8; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
-                if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                if(nx<0 || ny<0 || nx>=w || ny>=h) continue;
+                if(map[nx][ny] == 1 && !visited[nx][ny]){
                     visited[nx][ny] = true;
                     q.add(new int[]{nx,ny});
                 }
             }
-
         }
     }
-}
 
+
+}
