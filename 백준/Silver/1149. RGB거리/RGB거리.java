716fb39be87617c9c949065a2f78566d2df4a255
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-10-19 12:26:28 +0900

    [Silver I] Title: RGB거리, Time: 108 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,34 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     static StringTokenizer st;
+    static long[][] dp;
+    static int[][] cost;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] rgb = new int[n][n];
-        int[][] dp = new int[n][n];
+        n = Integer.parseInt(br.readLine());
+        dp = new long[n][3];
+        cost = new int[n][3];
         for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j<3; j++){
-                rgb[i][j] = Integer.parseInt(st.nextToken());
+                cost[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for(int i = 0; i<3; i++){
-            dp[0][i] = rgb[0][i];
+        for(int i = 0; i<n; i++){
+            Arrays.fill(dp[i],-1);
         }
-        for(int i =1; i<n; i++){
-            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + rgb[i][0];
-            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + rgb[i][1];
-            dp[i][2] = Math.min(dp[i-1][1],dp[i-1][0]) + rgb[i][2];
+        long result = Long.MAX_VALUE;
+        for(int c = 0; c<3; c++){
+            result = Math.min(result, memo(n-1, c));
         }
-        int min_value = Integer.MAX_VALUE;
-        for(int i = 0; i<3; i++){
-            min_value = Math.min(min_value,dp[n-1][i]);
-        }
-        System.out.println(min_value);
+        System.out.println(result);
 
-
+    }
+    static long memo(int idx, int color){
+        if(idx == 0){
+            return cost[idx][color];
+        }
+        if(dp[idx][color]!= -1){
+            return dp[idx][color];
+        }
+        long min = Integer.MAX_VALUE;
+        for(int i = 0; i<3; i++){
+            if(i == color) continue;
+            min = Math.min(min,(memo(idx-1,i)));
+        }
+        dp[idx][color] = min + cost[idx][color];
+        return dp[idx][color];
     }
 }
