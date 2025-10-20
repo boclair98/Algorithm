770c2535e131eff409507c3a525ab72a9146e994
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-10-20 16:00:31 +0900

    [level 2] Title: 요격 시스템, Time: 347.21 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,21 +1,25 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        // 끝나는 시간 기준으로 정렬
-        Arrays.sort(targets, (a,b) -> Integer.compare(a[1], b[1]));
-
-        int last = -1; // 마지막 요격 위치
-        for(int i = 0; i < targets.length; i++){
-            int start = targets[i][0];
-            int end = targets[i][1];
-            if(start >= last){
-                // 요격 필요
+        Arrays.sort(targets,(o1,o2)->{
+            return Integer.compare(o1[1],o2[1]);
+        });
+        
+        // for(int i = 0; i<targets.length; i++){
+        //     System.out.println(targets[i][0]+" "+targets[i][1]);
+        // }
+        int last = -1;
+        for(int[] a : targets){
+            int start = a[0];
+            int end = a[1];
+            if(start >=last){
                 answer++;
-                last = end; // 레이저 위치 갱신
+                last = end;
             }
         }
+        
+    
         return answer;
     }
 }
