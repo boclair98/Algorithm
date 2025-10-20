e193eea76f7608cb73dfe932da2633985982ea5f
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-10-20 12:37:44 +0900

    [Gold IV] Title: 운동, Time: 616 ms, Memory: 58296 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int v,e;
+    static int[][] dist;
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        dist = new int[v+1][v+1];
+        for(int i =1; i<=v; i++){
+            Arrays.fill(dist[i],INF);
+        }
+        for(int i = 0; i<e; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = c;
+        }
+
+        for(int k = 1; k<=v; k++){
+            for(int i = 1; i<=v; i++){
+                for(int j = 1; j<=v; j++){
+                    if (dist[i][k] == INF || dist[k][j] == INF) continue;
+                    if(dist[i][j] > dist[i][k] + dist[k][j]){
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+        int answer = INF;
+        for(int i = 1; i<=v; i++){
+            for(int j = 1; j<=v; j++){
+                if(i == j) continue;
+                if(dist[i][j]!= INF && dist[j][i] != INF){
+                    answer = Math.min(answer,dist[i][j]+dist[j][i]);
+                }
+            }
+        }
+        if(answer == INF){
+            System.out.println(-1);
+        }else{
+            System.out.println(answer);
+        }
+
+    }
+}
