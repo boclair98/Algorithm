c78dd9b37c41f56284be4db4b315fe3632cd96bf
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-10-24 17:45:16 +0900

    [Silver IV] Title: 토너먼트, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int n,a,b;
+    static int[] arr;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        while(a!=b){
+            a = (a+1) /2 ;
+            b = (b+1) /2;
+            count++;
+        }
+        System.out.println(count);
+
+
+    }
+}
