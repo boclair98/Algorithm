c6f587125c2e1354ea19a0c9cd415e74f102d2be
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-11-03 15:15:36 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1652 ms, Memory: 45484 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int n,m;
+    static int[][] map;
+    static int k;
+    static long total;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        k = Integer.parseInt(br.readLine());
+        for(int i = 0; i<k; i++){
+            total = 0;
+            st = new StringTokenizer(br.readLine());
+            int sx = Integer.parseInt(st.nextToken());
+            int sy = Integer.parseInt(st.nextToken());
+            int ex = Integer.parseInt(st.nextToken());
+            int ey = Integer.parseInt(st.nextToken());
+            for(int j = sx-1; j<ex; j++){
+                for(int k = sy-1; k<ey; k++){
+                    total+=map[j][k];
+                }
+            }
+            System.out.println(total);
+        }
+
+    }
+}
