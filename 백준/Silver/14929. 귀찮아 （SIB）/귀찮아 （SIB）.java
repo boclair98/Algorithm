3cb760e8ee66a0fb7a5fadaffbe1baee5159e576
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-11-03 14:09:33 +0900

    [Silver IV] Title: 귀찮아 (SIB), Time: 252 ms, Memory: 24380 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int n;
+    static long prefix,res;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i<n-1; i++){
+            prefix+=arr[i];
+            res+=(arr[i+1] * prefix);
+        }
+        System.out.println(res);
+
+    }
+}
