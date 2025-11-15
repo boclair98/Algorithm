a7e23b16cdfbf8639731afd0e64c899694ef607c
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-11-15 18:19:39 +0900

    [Gold III] Title: 파티, Time: 796 ms, Memory: 90852 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,11 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int n,x,m;
+    static StringTokenizer st;
+    static int n, m,x;
     static int[] dist;
     static List<List<int[]>> graph = new ArrayList<>();
-    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
@@ -15,44 +17,46 @@ public class Main {
         for(int i = 0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
-        for(int i = 0; i <m; i++){
+        for(int i = 0; i<m; i++){
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new int[]{b,c});
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph.get(x).add(new int[]{y,cost});
         }
         int max_dist = 0;
-        for(int i =1; i<=n; i++){
-            int total = dijk(i,x) + dijk(x,i);
-//            System.out.println(total);
-            max_dist = Math.max(max_dist, total);
+        for(int i = 1; i<=n; i++){
+            max_dist = Math.max(max_dist,dijkstra(i,x)+dijkstra(x,i));
         }
         System.out.println(max_dist);
     }
-    static int dijk(int start, int end){
+    static int dijkstra(int start, int end){
         dist = new int[n+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) ->{
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->{
             return Integer.compare(o1[1],o2[1]);
         });
         dist[start] = 0;
         pq.add(new int[]{start,0});
         while(!pq.isEmpty()){
             int[] cur = pq.poll();
-            int nowNode = cur[0];
-            int nowDist = cur[1];
-            if(dist[nowNode] < nowDist) continue;
-            for(int[] next : graph.get(nowNode)){
+            int node = cur[0];
+            int cost = cur[1];
+            if(dist[node] < cost) continue;
+            for(int[] next : graph.get(node)){
                 int nextNode = next[0];
-                int nextDist = next[1] + nowDist;
-                if(dist[nextNode] > nextDist){
-                    dist[nextNode] = nextDist;
-                    pq.add(new int[]{nextNode,nextDist});
+                int nextCost = cost + next[1];
+                if(dist[nextNode] > nextCost){
+                    dist[nextNode] = nextCost;
+                    pq.add(new int[]{nextNode,nextCost});
                 }
             }
         }
         return dist[end];
     }
+
+
+
+
 
 }
