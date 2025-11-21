a859d1d0363e27ca4dac6a9cd8c8cede40917103
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-11-21 16:44:40 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 592 ms, Memory: 56164 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int n,m;
+    static int[] parents;
+    static PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) ->{
+        return Integer.compare(o1[2],o2[2]);
+    });
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parents = new int[n+1];
+        for(int i = 1; i<=n; i++){
+            parents[i] = i;
+        }
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{a,b,c});
+        }
+        int total = 0;
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int distance = cur[2];
+            int pa = find(x);
+            int su = find(y);
+            if(pa != su){
+                union(x,y);
+                total+=distance;
+            }
+        }
+        System.out.println(total);
+    }
+    static int find(int x){
+        if(parents[x] == x) return x;
+        return parents[x] = find(parents[x]);
+    }
+    static void union(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x!=y){
+            parents[y] = x;
+        }
+    }
+}
