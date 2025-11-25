9e034572bdce8f25000ea427c59780ace2a9ed38
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-11-25 15:51:01 +0900

    [Silver IV] Title: 청기 백기, Time: 68 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int n;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n =Integer.parseInt(br.readLine());
+        System.out.println((long)Math.sqrt(n));
+    }
+}
