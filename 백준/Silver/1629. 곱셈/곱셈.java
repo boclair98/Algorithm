11a1bf9cfb53fbf6cc29dd7696053b3202cc6e46
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-12-01 23:52:30 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -2,26 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static StringTokenizer st;
+    static long a,b,c;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        long a = Long.parseLong(st.nextToken());
-        long b = Long.parseLong(st.nextToken());
-        long c = Long.parseLong(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+        c = Long.parseLong(st.nextToken());
         System.out.println(pow(a,b,c));
     }
 
-    private static long pow(long a, long b,long c) {
-        if( b == 1){
-            return a % c;
-        }
-        long temp = pow(a,b/2,c);
+    private static long pow(long a, long b, long c) {
+        if(b == 1) return a % c;
+        long val = pow(a, b / 2, c);
+        val = val * val  % c;
 
-        if(b % 2 == 1){
-            return (temp*temp % c ) * a % c;
-        }
-        return (temp * temp ) % c;
+        if(b % 2 == 0) return val;
+        return val * a  % c;
+
 
     }
-
 }
+
