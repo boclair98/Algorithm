c4007a63bc078947f6336958b41f445dc7197c68
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-12-02 11:57:44 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(dfs(n));
+    }
+
+    private static long dfs(int n) {
+        if(n<2) return 1;
+        return n * dfs(n-1);
+
+    }
+}
