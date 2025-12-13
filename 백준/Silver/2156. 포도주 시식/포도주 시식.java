62ac771df6801f31c13ccc3d64e193ac86805e98
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-12-13 14:08:19 +0900

    [Silver I] Title: 포도주 시식, Time: 96 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int n;
+    static int[] arr = new int[10050];
+    static int[][] dp = new int[10050][3];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(DFS(0,0));
+
+    }
+    static int DFS(int idx, int count){
+        if(idx == n) return 0;
+        if(dp[idx][count]!=-1) return dp[idx][count];
+        int result = 0;
+        result = DFS(idx+1,0);
+        if(count<2){
+            result = Math.max(result,DFS(idx+1,count+1)+arr[idx]);
+        }
+        return dp[idx][count] = result;
+    }
+
+}
