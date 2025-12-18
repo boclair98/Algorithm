d0e4f0ae0cdcf6836cd478574224e31320438b00
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-12-19 00:47:53 +0900

    [Silver V] Title: 돌 게임, Time: 64 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,32 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class Main {
-    static int[] dp ;
+public class Main {
+    static StringTokenizer st;
+    static int n;
+    static int[] dp = new int[5050];
+    static int recur(int idx){
+        if(idx < 0) return 1;
+        if(idx == 0) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        if(recur(idx - 1) == 0 || recur(idx - 3) == 0){
+            return dp[idx] = 1;
+        }else{
+            return dp[idx] = 0;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        dp = new int[1001];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 1;
-        for(int i = 4; i<=n; i++){
-            dp[i] = Math.min(dp[i-1],dp[i-3]) + 1;
-//            System.out.println(dp[i]);
+        n = Integer.parseInt(br.readLine());
+        for(int i = 0; i <= n; i++){
+            dp[i] = -1;
         }
-        if(dp[n] % 2 == 1){
+        if(recur(n) == 1){
             System.out.println("SK");
         }else{
             System.out.println("CY");
         }
     }
-
-
 }
+
