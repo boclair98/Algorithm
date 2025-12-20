a70bf3e7ee1d603fd088635ec0b83414eefbec7b
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-12-20 22:03:02 +0900

    [Silver III] Title: 퇴사, Time: 76 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,44 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static StringTokenizer st;
     static int n;
-    static int[] p;
-    static int[] t;
-    static int[] dp;
+    static int[][] arr = new int[15][2];
+    static int max_val = 0;
+    static void recur(int cur, int total){
+        if(cur > n) return;
+        if(cur == n){
+            max_val = Math.max(max_val, total);
+            return;
+        }
+        recur(cur+arr[cur][0], total+arr[cur][1]);
+        recur(cur+1, total);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(br.readLine());
-        p = new int[n+1];
-        t = new int[n+1];
-        dp = new int[n+2];
-        Arrays.fill(dp, -1);
-        for(int i = 1; i<=n; i++){
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            p[i] = Integer.parseInt(st.nextToken());
-            t[i] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(dfs(1));
-    }
-
-    private static int dfs(int day) {
-        if(day > n){
-            return 0;
-        }
-        if(dp[day]!=-1){
-            return dp[day];
-        }
-        int result = dfs(day+1);
-
-        if(day + p[day] -1 <=n){
-            result = Math.max(result, t[day]+dfs(day+p[day]));
-        }
-        return dp[day] = result;
-
-
+        recur(0, 0);
+        System.out.println(max_val);
     }
 }
+
