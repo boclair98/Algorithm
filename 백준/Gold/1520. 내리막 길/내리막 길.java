add1923555a22049e54f52302dbb3812ac0289b7
boclair98 <108774323+boclair98@users.noreply.github.com>
2025-12-21 01:09:14 +0900

    [Gold III] Title: 내리막 길, Time: 348 ms, Memory: 37304 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,42 +1,52 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static StringTokenizer st;
-    static int n, m;
-    static int[][] map = new int[505][505];
-    static int[][] dp = new int[505][505];
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    static int n, m ;
+    static int[][] dp;
+    static int[][] map;
+    static boolean[][] visited;
     static int[] dx = {1,0,-1,0};
     static int[] dy = {0,1,0,-1};
-    static int recur(int x, int y){
-        if(x == n-1 && y == m-1) return 1;
-        if(dp[x][y] != -1) return dp[x][y];
-        int res = 0;
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
-            if(map[nx][ny] < map[x][y]){
-                res+=recur(nx, ny);
-            }
-        }
-        return dp[x][y] = res;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st =  new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        dp = new int[n][m];
+        map = new int[n][m];
+        visited = new boolean[n][m];
         for(int i = 0; i<n; i++){
-            st =  new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             for(int j = 0; j<m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
-        for(int i = 0; i<505; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        System.out.println(recur(0, 0));
+        visited[0][0] = true;
+        System.out.println(dfs(0,0));
     }
+
+    static int dfs(int x, int y){
+        if(x == n-1 && y == m-1) return 1;
+        if(dp[x][y]!=-1) return dp[x][y];
+        dp[x][y] = 0;
+        for(int i = 0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
+            if(!visited[nx][ny] && map[nx][ny] < map[x][y]){
+                visited[nx][ny] = true;
+                dp[x][y]+=dfs(nx, ny);
+                visited[nx][ny] = false;
+            }
+        }
+        return dp[x][y];
+    }
+
 }
 
