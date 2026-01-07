f30353b71ff1fd3eebfb2ff5dcf433ac1119e8a4
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-01-07 16:36:52 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 5316 ms, Memory: 322096 KB -BaekjoonHub

---
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int n, k;
+    static List<Integer> list;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        k = Integer.parseInt(br.readLine());
+        int idx = n / k;
+
+        for(int i = 0; i < n; i+=idx){
+            list = new ArrayList<>();
+            for(int j = i; j<i+idx; j++){
+                list.add(arr[j]);
+            }
+            Collections.sort(list);
+            for (Integer num : list) {
+                System.out.print(num+" ");
+            }
+        }
+
+    }
+}
