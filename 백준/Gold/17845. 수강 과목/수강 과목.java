f4ba7d1aba41d49cd60377bdb8d25f3ea85a0494
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-01-23 15:36:18 +0900

    [Gold V] Title: 수강 과목, Time: 284 ms, Memory: 52488 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb;
+    static StringTokenizer st;
+    static int n,k;
+    static int[][] arr = new int[1010][2];
+    static int[][] dp = new int[1010][10050];
+    static int recur(int idx, int total){
+        if(total > n) return -1000000000;
+        if(idx == k) return 0;
+        if(dp[idx][total] != -1) return dp[idx][total];
+        int ans = 0;
+        ans = Math.max(ans,recur(idx+1,total+arr[idx][1])+arr[idx][0]);
+        ans = Math.max(ans,recur(idx+1,total));
+        return dp[idx][total] = ans;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < k; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < 1010; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        System.out.println(recur(0,0));
+    }
+}
