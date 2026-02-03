d99a9fa255d891d40929ef6fd0b0e865fff1e35e
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-02-03 22:26:46 +0900

    [Bronze I] Title: A/B - 2, Time: 68 ms, Memory: 11640 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println((double) n/m);
+
+    }
+}
