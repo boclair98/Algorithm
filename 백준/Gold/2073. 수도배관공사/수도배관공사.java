eeedfe47003a3c53307a00d3373b9c26bb966085
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-02-06 09:47:53 +0900

    [Gold IV] Title: 수도배관공사, Time: 892 ms, Memory: 168632 KB -BaekjoonHub

---
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int d,p;
+    static int[][] arr = new int[400][2];
+    static int[][]dp = new int[400][100001];
+    static int recur(int idx, int total){
+        if(total == d) return Integer.MAX_VALUE;
+        if(total > d) return 0;
+        if(idx == p) return 0;
+        if(dp[idx][total] != -1) return dp[idx][total];
+        int res = recur(idx+1, total);
+        int res1 = Math.min(arr[idx][1] , recur(idx+1, total + arr[idx][0]));
+        return dp[idx][total] = Math.max(res, res1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        d = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < p; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < 400; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        System.out.println(recur(0, 0));
+
+
+    }
+
+}
