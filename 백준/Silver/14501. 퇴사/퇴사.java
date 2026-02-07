82f2d0e2480e8d0c5b76d4fcdefdc94d64a4713f
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-02-07 13:55:09 +0900

    [Silver III] Title: 퇴사, Time: 60 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,29 +1,32 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import java.util.*;
 
 public class Main {
+    static StringBuilder sb;
     static StringTokenizer st;
     static int n;
-    static int[][] arr = new int[15][2];
-    static int[] dp = new int[200];
-    static int recur(int cur){
-        if(cur > n) return -1000000000;
-        if(cur == n) return 0;
-        if(dp[cur] !=-1) return dp[cur];
-        int a = recur(cur+arr[cur][0]) + arr[cur][1];
-        int b = recur(cur+1);
-        return dp[cur] = Math.max(a,b);
+    static int[][] arr = new int[20][2];
+    static int[] dp = new int[20];
+    static int recur(int idx){
+        if(idx == n) return 0;
+        if(idx > n) return Integer.MAX_VALUE;
+        if(dp[idx] != -1) return dp[idx];
+        int ans = recur(idx+1);
+        ans = Math.max(ans,recur(idx+arr[idx][0])+arr[idx][1]);
+        return dp[idx] = ans;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.fill(dp, -1);
+        Arrays.fill(dp,-1);
         System.out.println(recur(0));
     }
 }
-
