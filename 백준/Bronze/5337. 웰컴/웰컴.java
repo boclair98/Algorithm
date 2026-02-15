63da2b64f4c3bad6a0a4abe4655fbdd350dd0864
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-02-15 15:46:13 +0900

    [Bronze V] Title: 웰컴, Time: 60 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,18 @@
+import java.awt.color.ColorSpace;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+
+}
