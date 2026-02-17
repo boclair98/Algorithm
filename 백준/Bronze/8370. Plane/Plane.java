7343db4755e7fec3382d0d11945c7d1d22d81e33
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-02-17 23:13:20 +0900

    [Bronze V] Title: Plane, Time: 56 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,21 @@
+import java.awt.color.ColorSpace;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int a,b,c,d;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        System.out.println(a*b+c*d);
+
+    }
+}
