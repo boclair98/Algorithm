df74e1d9f05ad608ca2a2b5b0458e6de7554b514
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-05-02 12:58:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.13 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 86.4 MB, 시간: 0.09 ms
+메모리: 87.9 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 11일 01:07:54
+2026년 05월 02일 12:58:05
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,27 +1,41 @@
 import java.util.*;
-
 class Solution {
-    public List<Integer> solution(int n, String[] words) {
-        List<Integer> result = new ArrayList<>();
-        Set<String> usedWords = new HashSet<>();
-
-        usedWords.add(words[0]);
-        for (int i = 1; i < words.length; i++) {
-            String prev = words[i - 1];
-            String current = words[i];
-            if (usedWords.contains(current) || current.charAt(0) != prev.charAt(prev.length() - 1)) {
-                int person = (i % n) + 1;
-                int turn = (i / n) + 1;
-                result.add(person);
-                result.add(turn);
-                return result;
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        int num = 1;
+        int cnt = 0;
+        Set<String> set = new HashSet<>();
+        int idx = 0;
+        boolean flag = false;
+        while(idx < words.length){
+            if(cnt > n) {
+                cnt = 1;
+                num++;
             }
-            usedWords.add(current);
+            if(idx == 0){
+                set.add(words[idx]);
+                cnt++;
+            }else{
+                char end = words[idx-1].charAt(words[idx-1].length()-1);
+                char start = words[idx].charAt(0);
+                if(end != start){
+                    flag = true;
+                    break;
+                }
+                if(set.contains(words[idx])){
+                    flag = true;
+                    break;
+                }
+                set.add(words[idx]);
+            }
+            cnt++;
+            idx++;
         }
-
-        // 모든 사람이 규칙을 잘 지킨 경우
-        result.add(0);
-        result.add(0);
-        return result;
+        if(!flag){
+            return answer;
+        }
+        answer[0] = cnt;
+        answer[1] = num;
+        return answer;
     }
 }
