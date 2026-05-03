718364b96482252e99c06786a4d9a8bb55330f58
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-05-03 16:57:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.63 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,38 @@
 import java.util.*;
 class Solution {
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static boolean[][] visited;
+    static int n,m;
     public int solution(int[][] maps) {
         int answer = 0;
-        int m = maps[0].length;
-        int n = maps.length;
-        boolean[][] visited = new boolean[n][m];
-        int[][] dist = new int[n][m];
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
-        dist[0][0] = 1;
-        visited[0][0] = true;
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0});
-        
-        while (!q.isEmpty()){
-            int[] cur = q.poll();
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
+        return bfs(0,0,maps);
+    }
+    public static int bfs(int sx, int sy,int[][] maps){
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{sx,sy,0});
+        visited[sx][sy] = true;
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
             int x = cur[0];
             int y = cur[1];
-            for(int i = 0; i<4; i++){
+            int count = cur[2];
+            if(x == n-1 && y == m-1) return count+1;
+            for(int i = 0; i < 4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(nx<0 || nx >=n || ny < 0 || ny>=m)continue;
-                
-                if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                if(nx < 0 || nx>=n || ny < 0 || ny>=m) continue;
+                if(!visited[nx][ny] && maps[nx][ny] == 1){
                     visited[nx][ny] = true;
-                    dist[nx][ny] = dist[x][y] + 1;
-                    q.offer(new int[]{nx,ny});
+                    dq.add(new int[]{nx,ny,count+1});
                     
                 }
             }
         }
-        // for(int i = 0; i<n; i++){
-        //     for(int j = 0; j<m; j++){
-        //         System.out.print(dist[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        
-        if(dist[n-1][m-1] == 0){
-            return -1;
-        }
-        return dist[n-1][m-1];
+        return -1;
     }
+    
 }
