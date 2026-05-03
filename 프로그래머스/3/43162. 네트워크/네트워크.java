7a63a0cbb13b84ec13746795a378cbf0e84bc665
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-05-03 17:12:56 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,24 @@
-import java.util.*;
 class Solution {
     static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n+1];
-        
-        
-        for(int i = 0; i<n; i++){
+        visited = new boolean[n];
+        for(int i = 0; i < n; i++){
             if(!visited[i]){
                 answer++;
-                dfs(i,computers,n);
-                
+                dfs(i,n,computers);
             }
         }
-        
         return answer;
     }
-    public static void dfs(int idx, int[][] computers, int n){
+    static void dfs(int idx,int n, int[][]computers){
         visited[idx] = true;
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i < n; i++){
             if(!visited[i]){
                 if(computers[idx][i] == 1){
-                    dfs(i,computers,n);
+                    dfs(i,n,computers);
                 }
             }
         }
-        
     }
-    
 }
