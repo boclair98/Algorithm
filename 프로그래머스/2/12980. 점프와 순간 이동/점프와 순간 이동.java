cc6c239b086c40164252adc39d078be57d79bcd0
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-05-24 13:32:58 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -3,21 +3,14 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-        int start = 0;
-        while (true){
-            if(n == 0 ){
-                break;
-            }
-            if(n%2 == 0){
-                n = n /2;
-            }
-            if(n%2 == 1){
-                n = n /2 ;
+        while(true){
+            if(n < 1 ) break;
+            if(n % 2 == 1){
                 ans++;
             }
-            
+            n = n / 2;
         }
-        
+
         return ans;
     }
 }
