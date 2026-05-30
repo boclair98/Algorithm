cdc54e1e09b3bcfd7cd8151a48c333a2a17b0d51
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-05-30 13:22:30 +0900

    [level 2] Title: 올바른 괄호, Time: 7.01 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,28 +2,16 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-		Stack<Character> stack = new Stack<>();
-        for(int i = 0; i<s.length(); i++){
-            char word = s.charAt(i);
-            if(word =='('){
-                stack.add(word);
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(s.charAt(i));
             }else{
-                if(stack.size() == 0){
-            
-                    stack.add(word);
-                    break;
-                }else{
-                    if(stack.get(stack.size()-1) == '('){
-                        stack.pop();
-                    }
-                }
+                if(stack.size() == 0) return false;
+                else stack.pop();
             }
         }
-        if(stack.size() > 0){
-            answer = false;
-        }
-        
-
+        if(stack.size() > 0) return false;
         return answer;
     }
 }
