35527e36be9012774d3bbb86fccea5ae2d2e3f37
boclair98 <108774323+boclair98@users.noreply.github.com>
2026-06-03 13:42:12 +0900

    [level 2] Title: 괄호 회전하기, Time: 26.58 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 83.1 MB, 시간: 95.86 ms
+메모리: 63.8 MB, 시간: 26.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 15일 15:30:06
+2026년 06월 03일 13:42:09
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,49 +2,42 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        Deque<Character> deque = new ArrayDeque<>();
-        for(int i = 0; i<s.length(); i++){
-            deque.offer(s.charAt(i));
+        int cnt = 0;
+        Queue<Character> q = new LinkedList<>();
+        for(int i = 0; i < s.length(); i++){
+            q.add(s.charAt(i));
         }
-        int idx = 0;
-        int last = s.length();
-        int num = 0;
-        while (idx < last){
-            Stack<Character> stack = new Stack<>();
-            while (num < idx){
-                num++;
-                deque.offer(deque.pollFirst());
-            }
-            
-            idx++;
-            List<Character> list = new ArrayList<>(deque);
-            for(int i = 0; i< list.size(); i++){
-                if(list.get(i) ==')'|| list.get(i)==']'|| list.get(i)=='}'){
-                    if(stack.isEmpty()){
-                        stack.add(list.get(i));
-                        break;
-                    }
-                }
-                if(list.get(i)=='('||list.get(i)=='['||list.get(i)=='{'){
-                    stack.add(list.get(i));
-                }else if(list.get(i) == ')'){
-                    if(!stack.isEmpty()&&stack.get(stack.size()-1) =='('){
-                        stack.pop();
-                    }
-                }else if(list.get(i) == ']'){
-                    if(!stack.isEmpty()&&stack.get(stack.size()-1) =='['){
-                        stack.pop();
-                    }
-                }else if(list.get(i) == '}'){
-                    if(!stack.isEmpty()&&stack.get(stack.size()-1) =='{'){
-                        stack.pop();
-                    }
-                }
-            }
-            if(stack.isEmpty()){
-                answer++;
-            }
+        for(int i = 0; i < s.length(); i++){
+            q.add(q.poll());
+            if(check(q)) answer++;
         }
         return answer;
+    }
+    private boolean check(Queue<Character> q){
+        Stack<Character> stack = new Stack<>();
+        for(char s : q){
+            if(s =='(' || s == '[' || s =='{'){
+                stack.push(s);
+            }else{
+                if(stack.isEmpty()) return false;
+                if(s == ')'){
+                    if(stack.get(stack.size()-1) == '('){
+                        stack.pop();
+                    }
+                }
+                if(s == ']'){
+                    if(stack.get(stack.size()-1) == '['){
+                        stack.pop();
+                    }
+                }
+                if(s == '}'){
+                    if(stack.get(stack.size()-1) == '{'){
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        if(stack.isEmpty()) return true;
+        return false;
     }
 }
