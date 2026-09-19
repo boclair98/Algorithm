fae670e4c9fa4908b5b35bac1ed63eb9ec5a5f2f
TEDDYLEE <108774323+boclair98@users.noreply.github.com>
2026-09-19 13:51:43 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.05 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
 
 ### 성능 요약
 
-메모리: 88 MB, 시간: 0.06 ms
+메모리: 81.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 19일 13:45:47
+2026년 09월 19일 13:51:41
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public int solution(int storey) {
         int answer = 0;
@@ -10,29 +9,21 @@ class Solution {
         }
         
         for(int i = 0; i < number.length-1; i++){
-            int s = Integer.parseInt(number[i]);
-            int next = Integer.parseInt(number[i+1]); // 다음 자릿수 미리 확인
-            
-            // 1. s가 6 이상이거나, s가 5인데 다음 자릿수도 5 이상일 때 올림 수행
-            if(s > 5 || (s == 5 && next >= 5)){
-                answer += (10 - s);
-                number[i+1] = String.valueOf(next + 1); // 다음 자릿수에 +1 올림
-            } else {
-                answer += s;
+            int now  = Integer.parseInt(number[i]);
+            int next = Integer.parseInt(number[i+1]);
+            if(now > 5 || (now == 5 && next >= 5)){
+                answer+=(10-now);
+                number[i+1] = String.valueOf(Integer.parseInt(number[i+1])+1);
+            }else{
+                answer+=now;
             }
+        }    
+        int lastnumber = Integer.parseInt(number[number.length-1]);
+        if(lastnumber > 5){
+            answer+=(10-lastnumber+1);
+        }else{
+            answer+=lastnumber;
         }
-        
-        // 2. 마지막 자릿수 처리 (이 자릿수도 이전 루프에서 올라온 값 때문에 10이 될 수 있음)
-        int last = Integer.parseInt(number[number.length-1]);
-        if(last > 5) {
-            answer += (10 - last) + 1; // 10을 채우고 남은 수 + 맨 앞자리에 새로 생긴 '1' 추가
-        } else if(last == 5) {
-            // 마지막 자릿수가 딱 5라면 내리는 게 이득 (앞에 더이상 자릿수가 없으므로)
-            answer += 5;
-        } else {
-            answer += last;
-        }
-        
         return answer;
     }
 }
